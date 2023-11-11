d1e75a6de9996986306e434b159ce443cc03eef9
Fita Wegene <fi27ta@gmail.com>
2023-11-11 09:50:04 +0300

    Create NumIdenticalPairs.py

---
+++ NumIdenticalPairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        num_count = {}
+
+        for num in nums:
+            if num in num_count:
+                count += num_count[num]
+                num_count[num] += 1
+
+            else:
+                num_count[num] = 1
+
+
+        return count
